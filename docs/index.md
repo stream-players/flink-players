80761f1da96e305c5fb84760aab2608b130dfc06
fomky <fomky@foxmail.com>
2019-05-07 09:30:32 +0800

    init

---
+++ docs/index.md
@@ -0,0 +1,10 @@
+# StateBackends Players Module
+
+## How to use ?
+
+### Config Your Backends DB or RPC Service
+
+
+### Hight Lavel API 
+
+ 
---
+++ flink-statebackend-module/src/main/java/org/flinkplayers/flink/App.java
@@ -0,0 +1,13 @@
+package org.flinkplayers.flink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
